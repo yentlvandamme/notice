2355328694b0f81c7eb4abfe52d05f7d4eb62905
yentlvandamme <yentl.van.damme@gmail.com>
2024-02-04 21:51:44 -0500

    Parse JSON file

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
 func main () {
-	fmt.Println("Hello world")
+    var path string
+    args := os.Args
+
+    if len(args) > 1 {
+        path = args[1]
+    } else {
+        fmt.Fprintf(os.Stderr, "No path passed")
+        os.Exit(1)
+    }
+
+    fileData, err := os.ReadFile(path)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "No valid file path")
+        os.Exit(1)
+    }
+
+    var parsedJson map[string]interface{}
+    if err := json.Unmarshal(fileData, &parsedJson); err != nil {
+        fmt.Fprintf(os.Stderr, err.Error())
+        os.Exit(1)
+    }
 }
+
